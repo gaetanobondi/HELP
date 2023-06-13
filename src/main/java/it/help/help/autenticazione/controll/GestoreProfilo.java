afea55eb0241705e9b901750c6703512aca880f5
gaetanobondi <103059041+gaetanobondi@users.noreply.github.com>
2023-06-13 10:59:41 +0200

    Bugfix

--- src/main/java/it/help/help/autenticazione/controll/GestoreProfilo.java
+++ src/main/java/it/help/help/autenticazione/controll/GestoreProfilo.java
@@ -1,11 +1,15 @@
 package it.help.help.autenticazione.controll;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import it.help.help.entity.*;
 
 import it.help.help.utils.DBMS;
+import javafx.stage.Stage;
 
 public class GestoreProfilo {
 
@@ -22,11 +26,29 @@ public class GestoreProfilo {
     public Button buttonGestione;
     public Button buttonDonazioneAziendaPartner;
 
+    private final Responsabile responsabile;
+    private final Help help;
+
+    public GestoreProfilo(Responsabile responsabile, Help help) {
+        this.responsabile = responsabile;
+        this.help = help;
+    }
+
+    public GestoreProfilo() {
+        this.help = null;
+        this.responsabile = null;
+    }
+
+
+
     public void clickModificaDati(ActionEvent actionEvent) {
+
     }
 
     public void clickVisualizzaProfiloHelp(ActionEvent actionEvent) throws Exception {
-        Help help = DBMS.getHelp(1);
+        Parent root = FXMLLoader.load(getClass().getResource("/it/help/help/schermataProfiloPersonaleHelp.fxml"));
+        Stage window = (Stage) buttonVisualizzaProfiloHelp.getScene().getWindow();
+        window.setScene(new Scene(root));
     }
 
     public void clickVisualizzaPrevisioneDiDistribuzione(ActionEvent actionEvent) {
@@ -39,5 +61,14 @@ public class GestoreProfilo {
     }
 
     public void clickListaDonazioniRicevute(ActionEvent actionEvent) {
+    }
+
+    public void clickGestione(ActionEvent actionEvent) {
+    }
+
+    public void clickDonazioneAziendaPartner(ActionEvent actionEvent) {
+    }
+
+    public void clickLogout(ActionEvent actionEvent) {
     }
 }
